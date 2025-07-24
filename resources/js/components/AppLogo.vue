9ce844e5f8492d58ab9a98e541cb5b5181fb1f27
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-07-24 14:06:25 +0200

    gogo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,6 +3,8 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
+    <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
+
 
     </div>
 </template>
