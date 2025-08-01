287a13cf8b82df10cc9aa5d30cb662a90e9cac35
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-08-01 15:37:06 +0200

    erere

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -194,6 +194,18 @@
              </td>
 
 
+             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">Ajouter</a>
+                
+             </td>
+
+             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">Supprimer</a>
+                
+             </td>
+
         </tr>
 
         </tbody>
