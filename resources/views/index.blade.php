538169b6f9a642fd4b2ef33773621ccbdd19e92e
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-08-03 11:58:12 +0200

    utuyutu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
           <div class="shrink-0">
-            <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" class="size-8" />
+            <img src="" alt="Your Company" class="size-8" />
           </div>
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
