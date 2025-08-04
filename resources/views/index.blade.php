cf28dd9fbb9c9245b674d4815cbd4114b777d0e6
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-08-04 23:32:07 +0200

    nouveau

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -450,7 +450,7 @@
 <div class="bg-white py-24 sm:py-32">
   <div class="mx-auto grid max-w-7xl gap-20 px-6 lg:px-8 xl:grid-cols-3">
     <div class="max-w-xl">
-      <h2 class="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">Rencontrer Notre Equipe</h2>
+      <h2 class="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">Rencontre Notre Equipe</h2>
       <p class="mt-6 text-lg/8 text-gray-600">Nous sommes dynamique .</p>
     </div>
     <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
