640223d09876342ff2f66501794bbeeed6ccec85
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-08-04 14:11:25 +0200

    nouveau

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -50,7 +50,7 @@ import { Head, Link } from '@inertiajs/vue3';
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
           <div class="shrink-0">
-            <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" class="size-8" />
+            <img src="" alt="Your Company" class="size-8" />
           </div>
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
