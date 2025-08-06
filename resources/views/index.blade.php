37471132014a0b7d1d7b8979ae5bfdbc5a1ded8b
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-08-06 09:54:11 +0200

    lewalibari

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,6 +18,7 @@
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
           <div class="shrink-0">
+            <img src="" alt="Your Company" class="size-8" />
           </div>
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
