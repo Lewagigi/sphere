e721a46382727aa48ef86b28f4ad3c6690c7ddef
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-08-09 23:07:46 +0200

    utuyutuioopopllm

--- resources/js/pages/Uselist.vue
+++ resources/js/pages/Uselist.vue
@@ -4,9 +4,6 @@ import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 
-
-
-
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
@@ -23,19 +20,20 @@ const breadcrumbs: BreadcrumbItem[] = [
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
-
-
-
-                            <Head title="Welcome" />
-                            <h1>Welcome</h1>
-                            <p>Hello {{ user.name }}, welcome to your first Inertia app!</p>
-
-
-
-
+                    Je suis Administrateur
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <PlaceholderPattern />
 
 
                 </div>
-
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <PlaceholderPattern />
+                </div>
+            </div>
+            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                <PlaceholderPattern />
+            </div>
+        </div>
     </AppLayout>
 </template>
