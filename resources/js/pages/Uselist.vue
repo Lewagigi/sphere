0460f95719e0d03b84de5de0cf2d57132bb5a3fc
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-08-09 23:19:45 +0200

    utuyutuioopopllmlm

--- resources/js/pages/Uselist.vue
+++ resources/js/pages/Uselist.vue
@@ -4,6 +4,8 @@ import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 
+defineProps({ user: Object })
+
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
@@ -20,6 +22,13 @@ const breadcrumbs: BreadcrumbItem[] = [
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
+
+
+                    <Head title="Welcome" />
+                    <h1>Welcome</h1>
+                    <p>Hello {{ user.name }}, welcome to your first Inertia app!</p>
+
+
                     Je suis Administrateur
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
@@ -37,10 +46,6 @@ const breadcrumbs: BreadcrumbItem[] = [
         </div>
 
 
-        <Head title="Welcome" />
-        <h1>Welcome</h1>
-        <p>Hello {{ user.name }}, welcome to your first Inertia app!</p>
 
-        
     </AppLayout>
 </template>
