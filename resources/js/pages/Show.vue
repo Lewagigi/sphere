b12beb8f8dff4b1d380c843b751b9749590ef572
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-08-17 00:56:48 +0200

    eerrlmlm

--- resources/js/pages/Show.vue
+++ resources/js/pages/Show.vue
@@ -1,26 +1,10 @@
 <script setup>
 //import Layout from './Layout'
-import { Link, Head } from '@inertiajs/vue3'
+
 defineProps({ user: Object })
 </script>
 
 
 <template>
-  <Layout>
-    
-    <Head title="users" />
-    
 
-    <div v-for="user in users" :key="user.id">
-        {{ user.name }}
-      kfkkf
-      <Link :href="`/users/${user.id}`">
-           
-        {{ user.name }}
-
-      </Link>
-      
-      <div>{{ user.email }}</div>
-    </div>
-  </Layout>
 </template>
