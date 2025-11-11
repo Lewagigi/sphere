4fe7f9cafe44cb3f80e68bc4bd48f44454ef1c32
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-11-11 14:05:54 +0100

    rtrtrepppkkolmlmlmmmpp

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -154,7 +154,15 @@
 
 
 
-            <div class="flex items-stretch ...">  <div class="py-4">01</div>  <div class="py-12">02</div>  <div class="py-8">03</div></div>
+            <div class="flex items-stretch ...">
+                
+
+                <div class="py-4">01</div>
+                <div class="py-12">02</div>
+                <div class="py-8">03</div>
+
+
+            </div>
 
 
 
