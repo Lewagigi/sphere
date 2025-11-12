41c58ba5872308dad1babff034f2f0f67985fcf2
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-11-12 16:34:32 +0100

    gigi

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -191,57 +191,13 @@
   </div>
 
 
-    <h1>uuuuuuuu</h1>
-
-
-    <div class="grid grid-cols-3 gap-4">
-        <div class="visible ...">
-            <h2>ACCESSIBILITÉ</h2>
-            <p>
-                Rendre l'univers de la technologie accessible et comprehensible pour tous.
-            </p>
-
-        </div>
-
-        <div class="visible ...">
-            <h2>L'INNOVATION</h2>
-            <p>
-                Susciter des vocations et engendrer de futurs inovateurs,createurs et entrepreneurs
-                pour Dieu qui vont impacter leur géneration
-            </p>
-
-        </div>
-
-        <div>
-            <h2>CONSEILS ET CONNEXIONS</h2>
-            <p>
-                Connecter les personnes selon leurs besoins dans les nouvelles technologies.
-            </p>
-
-
-        </div>
-
-
-        <div>
-            <h2>LA FINALITE</h2>
-            <p>
-                Faire de l'evangelisation par l'influence attirer les ames à CHRIST.
-            </p>
-
-        </div>
 
 
 
-        <div>
-            <h2>Les membres impliquer à la prière</h2>
-            <p>
-                Les temps de prière
-            </p>
-
-        </div>
 
 
-    </div>
+
+
 
     <div class="flex flex-wrap items-center gap-x-4 gap-y-2">
     <p class="text-sm/6 text-gray-900">
@@ -267,9 +223,63 @@
 
 
 
-        <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
+        <! Include this script tag or install `@tailwindplus/elements` via npm: -->
 <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
-<div class="relative">
+
+        <h1>uuuuuuuu</h1>
+
+
+        <div class="grid grid-cols-3 gap-4">
+            <div class="visible ...">
+                <h2>ACCESSIBILITÉ</h2>
+                <p>
+                    Rendre l'univers de la technologie accessible et comprehensible pour tous.
+                </p>
+
+            </div>
+
+            <div class="visible ...">
+                <h2>L'INNOVATION</h2>
+                <p>
+                    Susciter des vocations et engendrer de futurs inovateurs,createurs et entrepreneurs
+                    pour Dieu qui vont impacter leur géneration
+                </p>
+
+            </div>
+
+            <div>
+                <h2>CONSEILS ET CONNEXIONS</h2>
+                <p>
+                    Connecter les personnes selon leurs besoins dans les nouvelles technologies.
+                </p>
+
+
+            </div>
+
+
+            <div>
+                <h2>LA FINALITE</h2>
+                <p>
+                    Faire de l'evangelisation par l'influence attirer les ames à CHRIST.
+                </p>
+
+            </div>
+
+
+
+            <div>
+                <h2>Les membres impliquer à la prière</h2>
+                <p>
+                    Les temps de prière
+                </p>
+
+            </div>
+
+
+        </div>
+
+
+        <div class="relative">
 
 
 
